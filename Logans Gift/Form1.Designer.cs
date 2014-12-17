e6160a21d9f047b6650ac9171b93b4ada487657b
logo448 <stenzellogan@gmail.com>
2014-12-17 17:50:44 -0600

    Deleted almost everything. Instead of asking for name in textbox I am using radio buttons.

--- Logans Gift/Form1.Designer.cs
+++ Logans Gift/Form1.Designer.cs
@@ -28,20 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
             this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(208, 12);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(239, 26);
-            this.textBox1.TabIndex = 0;
-            this.textBox1.Text = "First name";
-            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
             // 
             // label1
             // 
@@ -60,12 +53,45 @@
             this.label2.Size = new System.Drawing.Size(0, 20);
             this.label2.TabIndex = 2;
             // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(220, 9);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(61, 24);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Jeff";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(288, 8);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(74, 24);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Susie";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(369, 7);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(86, 24);
+            this.radioButton3.TabIndex = 5;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Maddie";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(467, 6);
+            this.button1.Location = new System.Drawing.Point(461, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 32);
-            this.button1.TabIndex = 3;
+            this.button1.Size = new System.Drawing.Size(75, 26);
+            this.button1.TabIndex = 6;
             this.button1.Text = "Done";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
@@ -76,9 +102,11 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(554, 468);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Logan\'s Gift";
             this.ResumeLayout(false);
@@ -88,9 +116,11 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
         private System.Windows.Forms.Button button1;
     }
 }
--- Logans Gift/Form1.cs
+++ Logans Gift/Form1.cs
@@ -17,56 +17,6 @@ namespace Logans_Gift
             InitializeComponent();
         }
 
-        /// <summary>
-        /// function that happens every time a keys is entered into the text box
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
-        {
-            // checks to see if the return key was hit
-            if (e.KeyChar == '\r')
-            {
-                string user_name = get_name();
-
-                Tuple<string, string> user_nick_names = check_name(user_name);
-
-                label2.Text = user_nick_names.Item1;
-            }
-        }
-
-        /// <summary>
-        /// function that happens every tim the first done button is clicked
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void button1_Click(object sender, EventArgs e)
-        {
-            string user_name = get_name();
-
-            Tuple<string, string> user_nick_names = check_name(user_name);
-
-            label2.Text = user_nick_names.Item1;
-        }
-
-        /// <summary>
-        /// gets the text from the name text box
-        /// </summary>
-        /// <returns>
-        /// returns a string
-        /// </returns>
-        private string get_name()
-        {
-            // sets the value of the textbox text to a variable
-            string textbox1_text = textBox1.Text;
-
-            // make all leters lowercase
-            textbox1_text = textbox1_text.ToLower();
-
-            // return the text from the textbox
-            return textbox1_text;
-        }
-
         private Tuple<string, string> check_name(string name)
         {
             switch (name)
@@ -80,5 +30,7 @@ namespace Logans_Gift
             }
             return null;
         }
+
+
     }
 }
