f04f8355e1af1f499ca1b6881085c82115305eb8
logo448 <stenzellogan@gmail.com>
2014-12-17 11:42:36 -0600

    Now the program gets the users two nick names.

--- Logans Gift/Form1.cs
+++ Logans Gift/Form1.cs
@@ -27,21 +27,35 @@ namespace Logans_Gift
             // checks to see if the return key was hit
             if (e.KeyChar == '\r')
             {
-                string user_name = get_text();
+                string user_name = get_name();
+
+                Tuple<string, string> user_nick_names = check_name(user_name);
+
+                label2.Text = user_nick_names.Item1;
             }
         }
 
         /// <summary>
-        /// function that happens every time the done button is clicked
+        /// function that happens every tim the first done button is clicked
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void button1_Click(object sender, EventArgs e)
         {
-            string user_name = get_text();
+            string user_name = get_name();
+
+            Tuple<string, string> user_nick_names = check_name(user_name);
+
+            label2.Text = user_nick_names.Item1;
         }
 
-        private string get_text()
+        /// <summary>
+        /// gets the text from the name text box
+        /// </summary>
+        /// <returns>
+        /// returns a string
+        /// </returns>
+        private string get_name()
         {
             // sets the value of the textbox text to a variable
             string textbox1_text = textBox1.Text;
@@ -49,11 +63,22 @@ namespace Logans_Gift
             // make all leters lowercase
             textbox1_text = textbox1_text.ToLower();
 
-            // debug
-            label2.Text = textbox1_text;
-
             // return the text from the textbox
             return textbox1_text;
+        }
+
+        private Tuple<string, string> check_name(string name)
+        {
+            switch (name)
+            {
+                case "jeff":
+                    return Tuple.Create("Snow Papa", "Jeffamund");
+                case "susie":
+                    return Tuple.Create("Snow Angel", "Momamund");
+                case "maddie":
+                    return Tuple.Create("Snow Monster", "Madamund");
+            }
+            return null;
         }
     }
 }
